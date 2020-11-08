ae1fd761b259a8ba2a4a037c688e6f98b67da644
Shamus_Rezol <73869295+Shamus-Rezol@users.noreply.github.com>
2020-11-08 18:14:13 +0700

    Обновление отказоустойчивости preferences.json.

--- preferences.py
+++ preferences.py
@@ -1,9 +1,13 @@
 import os.path, json
 
-DEFAULT=dict(groups=list(), departaments=list())
+DEFAULT=dict(groups=["11исип202"], departaments=["информ"])
 
 class Preferences(object):
 	"""Объект конфигурации предпочтений.
+
+	Методы
+	------
+	Метод load_defaults скрыт и должен игнорироваться.
 
 	Аттрибуты
 	---------
@@ -21,20 +25,33 @@ class Preferences(object):
 			Полный путь к файлу предпочтений.
 		"""
 		if os.path.isfile(file):
-			with open(file, 'r', encoding="utf-8") as fp:
-				data=json.load(fp)
+			try:
+				with open(file, 'r', encoding="utf-8") as fp:
+					data=json.load(fp)
 
-				if "groups" in data.keys():
-					self.groups=data["groups"]
-				else:
-					self.groups=DEFAULT["groups"]
+					if "groups" in data.keys():
+						self.groups=data["groups"]
+					else:
+						self.groups=DEFAULT["groups"]
 
-				if "departaments" in data.keys():
-					self.departaments=data["departaments"]
-				else:
-					self.departaments=DEFAULT["departaments"]
+					if "departaments" in data.keys():
+						self.departaments=data["departaments"]
+					else:
+						self.departaments=DEFAULT["departaments"]
+			except:
+				import panic, traceback
+				whats="[ preferences.json ] Ошибка чтения файла предпочтений:"
+				message="Файл предпочтений был сброшен к настройкам по "
+				message+="умолчанию из-за следующей ошибки:\n\n"
+				message+=traceback.format_exc()
+				panic.info(whats, message, (232, 199), (51, 232))
+				self.load_defaults(file)
 		else:
-			with open(file, 'w', encoding="utf-8") as fp:
-				json.dump(DEFAULT, fp, ensure_ascii=False, indent=4)
-				self.groups=DEFAULT["groups"]
-				self.departaments=DEFAULT["departaments"]
+			self.load_defaults(file)
+
+	def load_defaults(self, file):
+		"""Загружает настройки по умолчанию."""
+		with open(file, 'w', encoding="utf-8") as fp:
+			json.dump(DEFAULT, fp, ensure_ascii=False, indent=4)
+			self.groups=DEFAULT["groups"]
+			self.departaments=DEFAULT["departaments"]
